db924e80ed2c749be3fcaa80bf9824f3dae17244
ndt080 <68751397+ndt080@users.noreply.github.com>
2021-04-17 23:38:02 +0300

    Add z2: Weather

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Andrei Petrov on 17.04.21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
